28bdfdba4eb33d2fae9fc447c9bc237678ecf3f5
geethmaduranga <geeth3003@gmail.com>
2017-10-28 16:41:09 +0530

    top-dummy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserDummyComponent } from './user-dummy/user-dummy.component';
+import { TopDummyComponent } from './top-dummy/top-dummy.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    UserDummyComponent
+    UserDummyComponent,
+    TopDummyComponent
   ],
   imports: [
     BrowserModule
